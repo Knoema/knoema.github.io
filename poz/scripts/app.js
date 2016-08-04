3519622962a82890f4b5d5ceb69db6c6cbaa719e
Karpenko Anton <akarpenko@knoema.com>
2016-08-04 15:35:03 +0500

    AppId was updated

--- poz/scripts/app.js
+++ poz/scripts/app.js
@@ -7,7 +7,7 @@ $(function () {
 	/* Authentication via access token  */
 	var params = Knoema.Helpers.parseHashParams();
 	if (params == null)
-		Knoema.Helpers.getAccessToken('Ysyd9Tw', window.location, false, 'read_resources');
+		Knoema.Helpers.getAccessToken('bOrx550', window.location, false, 'read_resources');
 	else {
 		if (params["access_token"] != undefined)
 			access_token = params["access_token"];
