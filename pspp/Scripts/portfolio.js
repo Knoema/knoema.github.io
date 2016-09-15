58c79ba9c2990b5de784f3bf086cdd2c183196df
Karpenko Anton <akarpenko@knoema.com>
2016-09-15 17:54:26 +0500

    CLI-3693, CLI-3605

---
+++ pspp/Scripts/portfolio.js
@@ -0,0 +1,119 @@
+﻿/// <reference path="typings/jquery.d.ts"/>
+
+(function () {
+	var host = 'http://pspp.knoema.com';
+
+	var projectsDataset = 'sardyld';
+
+	var projectData = [];
+	var projectColumns = [];
+
+	var objectTypeIndex = -1;
+	var nameIndex = -1;
+	var ppIndex = -1;
+	var codeIndex = -1;
+
+	var portfolio = (function () {
+		function portfolio() {
+		};
+
+		portfolio.prototype.run = function () {
+
+			var _this = this;
+
+			this.getProjects().done(function (projectData) {
+
+				_this.projectData = projectData.data;
+				_this.projectColumns = projectData.columns;
+
+				for (var i = 0; i < _this.projectColumns.length; i++) {
+
+					var name = _this.projectColumns[i].name;
+
+					if (name == 'Projet/Reforme') _this.objectTypeIndex = i;
+					if (name == 'Nom Projet') _this.nameIndex = i;
+					if (name == 'Numéro du projet phare / numéro de la réforme phare') _this.ppIndex = i;
+					if (name == 'Database Code') _this.codeIndex = i;
+				}
+
+				$($('.icon-item').get(0)).trigger('click');
+			});
+
+			$('.icon-item').on('click', function () {
+
+				$('.icon-item').removeClass('active');
+				$(this).addClass('active');
+				var name = $(this).data('p-name');
+				$('.p-name').text(name);
+
+				var project = _this.getProgectsByPP($(this).data('p'));
+
+				var $ils = [];
+				for (var i = 0; i < project.length; i++) {
+					$ils.push($('<li>', {
+						text: project[i].name,
+						'data-code': project[i].code
+					}).prepend($('<img>', { src: './img/ico-map.png' })));
+				}
+
+				$('.left-part ul').empty().append($ils);
+
+				$('.left-part ul li').on('click', function () {
+
+					$('.left-part ul li').removeClass('active');
+					$(this).addClass('active');
+				});
+			})
+		};
+
+		portfolio.prototype.getProgectsByPP = function (pp) {
+
+			var res = [];
+
+			for (var i = 0; i < this.projectData.length / this.projectColumns.length; i++) {
+
+				var offset = i * this.projectColumns.length;
+
+				//only for reforms
+				if (this.projectData[offset + this.objectTypeIndex] != 'P')
+					continue;
+
+				if (this.projectData[offset + this.ppIndex] != pp)
+					continue;
+
+				res.push({
+					code: this.projectData[offset + this.codeIndex],
+					name: this.projectData[offset + this.nameIndex]
+				});
+			}
+
+			return res;
+		};
+
+		portfolio.prototype.getProjects = function () {
+
+			var url = host + '/api/1.0/data/details?client_id=EZj54KGFo3rzIvnLczrElvAitEyU28DGw9R73tif&page_id=' + projectsDataset;
+			var data = {
+				'Header': [],
+				'Stub': [],
+				'Filter': [{
+					'DimensionId': 'measure',
+					'DimensionName': 'Measure',
+					'DatasetId': projectsDataset,
+					'Members': ['5679690', '5679700', '5679710']
+				}],
+				'Frequencies': [],
+				'Dataset': projectsDataset,
+				'Segments': null,
+				'MeasureAggregations': null
+			};
+
+			return $.post(url, data);
+		};
+
+		return portfolio;
+	})();
+
+	var r = new portfolio();
+	r.run();
+})();
--- pspp/Scripts/reforms.js
+++ pspp/Scripts/reforms.js
@@ -1,21 +1,25 @@
-/// <reference path="typings/jquery.d.ts"/>
+﻿/// <reference path="typings/jquery.d.ts"/>
 
 (function () {
-	//var host = 'http://pspp.knoema.com';
-	var host = 'https://beta.knoema.org';
+	var host = 'http://pspp.knoema.com';
+	//var host = 'https://beta.knoema.org';
 
-	var projectsDataset = 'hoxuwvc';
+	var projectsDataset = 'sardyld';
 
 	var projectData = [];
 	var projectColumns = [];
 
+	var codeIndex = -1;
 	var objectTypeIndex = -1;
 	var nameIndex = -1;
 	var deadlineIndex = -1;
+	var categoryIndex = -1;
+	var typeIndex = -1;
 
 	var reforms = (function () {
 		function reforms() {
 		};
+
 		reforms.prototype.run = function () {
 
 			var _this = this;
@@ -32,14 +36,19 @@
 					if (name == 'Projet/Reforme') _this.objectTypeIndex = i;
 					if (name == 'Nom Projet') _this.nameIndex = i;
 					if (name == 'Deadline') _this.deadlineIndex = i;
+					if (name == 'Database Code') _this.codeIndex = i;
+					if (name == 'Catégorie des réformes') _this.categoryIndex = i;
+					if (name == 'Type') _this.typeIndex = i;
 				}
 
-				_this.fillTable('full');
+				_this.fillTable('total');
 			});
 
 			$('.reforms-container ul li').on('click', function () {
 				$('.reforms-container ul li').removeClass('active');
 				$(this).addClass('active');
+
+				_this.fillTable($(this).data('tab-name'));
 			});
 
 			reforms.prototype.fillTable = function (status) {
@@ -50,17 +59,75 @@
 
 					var offset = i * _this.projectColumns.length;
 
-					//only for projects
+					//only for reforms
 					if (_this.projectData[offset + _this.objectTypeIndex] != 'R')
 						continue;
 
-					$trs.push($('<tr>')
+					if (status != 'total')
+						continue;
+
+					var code = _this.projectData[offset + _this.codeIndex];
+
+					$trs.push($('<tr>', { 'data-code': code })
 						.append($('<td>', { text: _this.projectData[offset + _this.nameIndex] }))
 						.append($('<td>', { text: _this.projectData[offset + _this.deadlineIndex] }))
+						.append($('<td>').append($('<img>', { src: './img/icon-arrow.png', 'class': 'arrow-icon' })))
 						);
 				}
 
-				$('.left-part').find('table tbody').append($trs);
+				if (status == 'total')
+					$('#count-total').text($trs.length);
+
+				_this.clearReformData();
+
+				$('.left-part').find('table tbody').empty().append($trs);
+
+				$('.reforms-table .left-part table tbody tr').off('click');
+				$('.reforms-table .left-part table tbody tr').on('click', function () {
+					$('.reforms-table .left-part table tbody tr').removeClass('active');
+					$('.reforms-table .left-part table tbody tr img.arrow-icon').hide();
+					$(this).addClass('active');
+					$(this).find('img.arrow-icon').show();
+
+					_this.displayReformData($(this).data('code'));
+				});
+			};
+
+			reforms.prototype.displayReformData = function (code) {
+
+				_this.clearReformData();
+
+				for (var i = 0; i < _this.projectData.length / _this.projectColumns.length; i++) {
+
+					var offset = i * _this.projectColumns.length;
+
+					//only for reforms
+					if (_this.projectData[offset + _this.objectTypeIndex] != 'R')
+						continue;
+
+					if (_this.projectData[offset + _this.codeIndex] != code)
+						continue;
+
+					var data = [
+						_this.projectData[offset + _this.nameIndex],
+						_this.projectData[offset + _this.deadlineIndex],
+						_this.projectData[offset + _this.categoryIndex],
+						_this.projectData[offset + _this.typeIndex],
+					];
+
+					$('.right-part .general-info tr').each(function (index, item) {
+
+						$($(item).find('td')[1]).text(data[index]);
+					});
+				}
+			};
+
+			reforms.prototype.clearReformData = function () {
+
+				$('.right-part .general-info tr').each(function (index, item) {
+
+					$($(item).find('td')[1]).text('');
+				});
 			};
 		};
 
