c5f76bfabc0080e35051903bc2595bbe7e2e4934
Karpenko Anton <akarpenko@knoema.com>
2016-10-03 11:40:40 +0500

    CLI-3745 the index page

--- explorim/scripts/map.js
+++ explorim/scripts/map.js
@@ -1,4 +1,14 @@
+var access_token = "";
+
 (function () {
+
+	var params = Knoema.Helpers.parseHashParams();
+	if (params == null)
+		Knoema.Helpers.getAccessToken('Ysyd9Tw', window.location, false, 'publish');
+	else {
+		if (params["access_token"] != undefined)
+			access_token = params["access_token"];
+	}
 
 	var app = (function () {
 
