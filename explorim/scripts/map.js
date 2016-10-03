ece13cf2faa8b257d3efdf33a746d76e5cdc0f7b
suschevsky <suschevsky@knoema.com>
2016-10-03 16:14:28 +0500

    CLI-3745 Fix link to map.html

--- explorim/scripts/map.js
+++ explorim/scripts/map.js
@@ -3,40 +3,41 @@ var access_token = "";
 (function () {
 
 	var params = Knoema.Helpers.parseHashParams();
-	if (params == null)
+	if (params == null) {
 		Knoema.Helpers.getAccessToken('Ysyd9Tw', window.location, false, 'publish');
+	}
 	else {
-		if (params["access_token"] != undefined)
+		if (typeof params["access_token"] !== 'undefined') {
 			access_token = params["access_token"];
+		}
 	}
 
 	var app = (function () {
-
 		function app() {
-		}
-
-		app.prototype.init = function () {
-
+			this._map = null;
+			this.geoplaygroundId = 'zabecdg';
 			this._layers = {};
 			this._currentLayer = null;
+		}
+		app.prototype.init = function () {
 
 			this._map = new google.maps.Map($('#map-container')[0], {
 				mapTypeControl: false,
 				zoom: 6,
-				center: { lat: 20.215167, lng: -10.777588 }
+				center: {
+					lat: 20.215167,
+					lng: -10.777588
+				}
 			});
 
-			var _this = this;
+			var self = this;
 			$('#filters>ul li').on('click', function () {
-
 				$('#filters>ul li').removeClass('active');
 				$(this).addClass('active');
 			});
 
 			$('.item-content li').on('click', function () {
-
 				var input = $(this).find('input');
-
 				if (input.prop('checked')) {
 					input.prop('checked', false);
 				}
@@ -45,7 +46,7 @@ var access_token = "";
 					input.prop('checked', true);
 
 					var layerId = input.val();
-					_this.loadLayer(layerId);
+					self.loadLayer(layerId);
 				}
 			});
 		};
@@ -54,9 +55,8 @@ var access_token = "";
 	})();
 
 	app.prototype.loadLayer = function (layerId) {
-
-		var _this = this;
-		this.removeLayer(this._currentLayer);
+		var self = this;
+		this.cleanLayer(this._currentLayer);
 
 		if (this._layers[layerId]) {
 			this._layers[layerId].load();
@@ -64,9 +64,9 @@ var access_token = "";
 		else {
 			var infoWindow = new google.maps.InfoWindow();
 			var layer = new GeoPlayground.Layer({
-				map: _this._map,
+				map: self._map,
 				layerId: layerId,
-				geoPlaygroundId: 'zabecdg',
+				geoPlaygroundId: self.geoplaygroundId,
 				infoWindow: infoWindow
 			}, function (layerData) {
 			});
@@ -77,11 +77,11 @@ var access_token = "";
 
 		this._currentLayer = layerId;
 	};
-	app.prototype.removeLayer = function (layerId) {
 
-		if (!this._layers[layerId])
+	app.prototype.cleanLayer = function (layerId) {
+		if (!this._layers[layerId]) {
 			return;
-
+		}
 		this._layers[layerId].clean();
 	};
 
